f4d8a23de8aafb18c4db21fdd701cdb26e804ae8
Rashadbm206 <101752000+Rashadbm206@users.noreply.github.com>
2023-10-08 14:55:58 -0400

    Update queue.c
    Verified qopen, qremove work as expected
    Debugging few bugs in qapply and qput

--- queue.c
+++ queue.c
@@ -1,75 +1,82 @@
-/* queue.c --- 
- * 
- * 
- * Author: Amelia H. Evans
- * Created: Tue Oct  3 10:23:09 2023 (-0400)
- * Version: 
- * 
- * Description: 
- * 
- */
-#include "queue.h"
-#include <stdint.h>                                                                                                                                                                                         
-#include <stdlib.h> // For malloc function                                                                                                                                                                  
-#include <stdbool.h>                                                                                                                                                                                        
-                                                                                                                                                                                                            
-/* the queue representation is hidden from users of the module */                                                                                                                                           
-//typedef void queue_t;                                                                                                                                                                                     
-                                                                                                                                                                                                                                                                                                                                                                                                        
-// Define custom data structure for element                                                                                                                                                                 
-typedef struct node{   
-  void *data;                                                                                                                                                                                      
-  struct node *previous;                                                                                                                                                                                    
-  struct node *next;                                                                                                                                                                                        
-}node_t;                                                                                                                                                                                                    
- 
- // Define custom data stucture for queue                                                                                                                                                                    
-typedef struct queue{                                                                                                                                                                                       
- node_t *front;                                                                                                                               
- node_t *back;                                                                                                                                                                                       
-}queue_t;                                                                                                                                                                                                   
-            
-                                                                                                                                                                                                                       
-/* create an empty queue */                                                                                                                                                                                 
-queue_t* qopen(void){                                                                                                                                                                                       
-                                                                                                                                                                                                            
-  // Allocate space for one instance of structure                                                                                                                                                           
-  queue_t *qp = (queue_t*)malloc(sizeof(queue_t));                                                                                                                                                          
-                                                                                                                                                                                                            
-  // Assign front and back for new queue                                                                                                                                                                    
-  qp -> front = NULL;                                                                                                                                                                                       
-  qp -> back = NULL;                                                                                                                                                                                        
-                                                                                                                                                                                                            
-  return qp;                                                                                                                                                                                                
-}                                                                                                                                                                                                           
-                                                                                                                                                                                                            
-/* deallocate a queue, frees everything in it */                                                                                                                                                            
-void qclose(queue_t *qp);                                                                                                                                                                                   
+/*                                                                                           
+ * queue.c -- public interface to the queue module                                           
+ */                                                                                          
+#include <stdint.h>                                                                          
+#include <stdlib.h> // For malloc function                                                   
+#include <stdbool.h>                                                                         
+                                                                                             
+/* the queue representation is hidden from users of the module */                            
+//typedef void queue_t;                                                                      
+                                                                                             
+// Define custom data structure for element                                                  
+typedef struct node{                                                                         
+  void* previous;                                                                            
+  void* next;                                                                                
+}node_t;                                                                                     
+                                                                                             
+// Define custom data stucture for queue                                                     
+typedef struct queue{                                                                        
+  struct node* front;                                                                        
+  struct node* back;                                                                         
+}queue_t;                                                                                    
+                                                                                             
+/* create an empty queue */                                                                  
+queue_t* qopen(void){                                                                        
+                                                                                             
+  // Allocate space for one instance of structure                                            
+  queue_t *qp = (queue_t*)malloc(sizeof(queue_t));                                           
+                                                                                             
+  // Assign front and back for new queue                                                     
+  qp -> front = NULL;                                                                        
+  qp -> back = NULL;                                                                         
+                                                                                             
+  return qp;                                                                                 
+}                                                                                            
+                                                                                             
+/* deallocate a queue, frees everything in it */                                             
+void qclose(queue_t *qp){                                                                    
+  free(qp);                                                                                  
+}                                                                                                                                                                                  
                                                                                                                                                                                                             
 /* put element at the end of the queue                                                                                                                                                                      
  * returns 0 is successful; nonzero otherwise                                                                                                                                                               
  */                                                                                                                                                                                                         
-int32_t qput(queue_t *qp, void *elementp){                                                                                                                                                                  
-  if ((qp->front == NULL) && (qp->back == NULL)){                                                                                                                                                           
-    qp->front = elementp;                                                                                                                                                                                   
-    qp->back = elementp;                                                                                                                                                                                    
-  }                                                                                                                                                                                                         
-                                                                                                                                                                                                            
-  else{                                                                                                                                                                                                     
-    element->previous = qp->back;                                                                                                                                                                           
-    element->next = NULL;                                                                                                                                                                                   
-    qp -> back = elementp;                                                                                                                                                                                  
-  }                                                                                                                                                                                                         
+int32_t qput(queue_t *qp, void *elementp){                                                   
+  if ((qp->front == NULL) && (qp->back == NULL)){                                            
+    qp->front = elementp;                                                                    
+    qp->back = elementp;                                                                     
+  }                                                                                          
+                                                                                             
+  else{                                                                                      
+    qp -> back = elementp;                                                                   
+    //node_t element = (node_t) elementp;                                                    
+    //element.previous = qp->back;                                                           
+    //element.next = NULL;                                                                   
+  }                                                                                          
+}                                                                                            
+                                                                                             
+/* get the first first element from queue, removing it from the queue */                     
+void* qget(queue_t *qp){                                                                     
+  if ((qp->front != NULL) && (qp->back != NULL)){                                            
+    void *removed = qp->front;                                                               
+                                                                                             
+    node_t *current;                                                                         
+    current = qp->front;                                                                     
+    qp->front = current->next;                                                               
+                                                                                             
+    return removed;                                                                          
+  }                                                                                          
+}                                                                                            
+                                                                                             
+/* apply a function to every element of the queue */                                         
+void qapply(queue_t *qp, void (*fn)(void* elementp)){                                        
+  node_t *current = qp->front;                                                               
+  do{                                                                                        
+    fn(current);                                                                             
+    current = current->next;                                                                 
+  } while (current != qp->back);                                                             
+                                                                                             
 } 
-
-
-/* apply a function to every element of the queue */
-void qapply(queue_t *qp, void (*fn)(void* elementp)){
-	while (current->next != NULL){
-		fn(qp); 
-		qp -> next = next; 
-	}
-}
 
 /* search a queue using a supplied boolean function
  * skeyp -- a key to search for
