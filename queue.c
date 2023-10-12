cabaa227b7479619dd42040fe4fa2c017a13cbf9
ameliaevans24 <133508655+ameliaevans24@users.noreply.github.com>
2023-10-12 11:19:35 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -42,7 +42,7 @@ void qclose(queue_t *qp){
     free(current);                                                              
     current = temp;                                                             
   }                                                                             
-  free(qp);                                                                    \
+  free(qp);                                                                    
                                                                                 
 }                                                                                                                                                                                  
                                                                                                                                                                                                             
