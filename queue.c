5f40c2e9b0bc248c70aed7448b79f6c7b1bb6aa0
ameliaevans24 <133508655+ameliaevans24@users.noreply.github.com>
2023-10-12 13:03:50 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,7 +1,8 @@
 /*                                                                                           
  * queue.c -- public interface to the queue module                                           
  */                                                                                          
-#include <stdint.h>                                                                          
+#include <stdint.h>
+#include "queue.h"
 #include <stdlib.h> // For malloc function                                                   
 #include <stdbool.h>                                                                         
                                                                                              
@@ -16,33 +17,34 @@ typedef struct node{
                                                                                              
 // Define custom data stucture for queue                                                     
 typedef struct queue{                                                                        
-  struct node* front;                                                                        
-  struct node* back;                                                                         
-}queue_t;                                                                                    
+struct node* front;                                                                        
+struct node* back;                                                                         
+}iqueue_t;                                                                                    
                                                                                              
 /* create an empty queue */                                                                  
 queue_t* qopen(void){                                                                        
                                                                                              
   // Allocate space for one instance of structure                                            
-  queue_t *qp = (queue_t*)malloc(sizeof(queue_t));                                           
+  iqueue_t *iqp = (iqueue_t*)malloc(sizeof(iqueue_t));                                           
                                                                                              
   // Assign front and back for new queue                                                     
-  qp -> front = NULL;                                                                        
-  qp -> back = NULL;                                                                         
+  iqp -> front = NULL;                                                                        
+  iqp -> back = NULL;                                                                         
                                                                                              
-  return qp;                                                                                 
+  return (queue_t*) iqp;                                                                                 
 }                                                                                            
                                                                                              
 /* deallocate a queue, frees everything in it */                                             
-void qclose(queue_t *qp){                                                       
-  node_t *current = qp->front;                                                  
+void qclose(queue_t *qp){   
+  iqueue_t *iqp = (iqueue_t*) qp;                                                     
+  node_t *current = iqp->front;                                                  
   node_t *temp = NULL;                                                          
   while (current != NULL) {                                                     
     temp = current->next;                                                       
     free(current);                                                              
     current = temp;                                                             
   }                                                                             
-  free(qp);                                                                    
+  free(iqp);                                                                    
                                                                                 
 }                                                                                                                                                                                  
                                                                                                                                                                                                             
@@ -50,7 +52,8 @@ void qclose(queue_t *qp){
  * returns 0 is successful; nonzero otherwise                                                                                                                                                               
  */                                                                                                                                                                                                         
 int32_t qput(queue_t *qp, void *elementp) {
-    if (qp == NULL || elementp == NULL) {
+  iqueue_t *iqp = (iqueue_t*) qp;  
+    if (iqp == NULL || elementp == NULL) {
         return -1; // Invalid input
     }
 
@@ -62,14 +65,14 @@ int32_t qput(queue_t *qp, void *elementp) {
     new_node->next = NULL;
     new_node->elementp = elementp;
 
-    if (qp->front == NULL && qp->back == NULL) {
+    if (iqp->front == NULL && iqp->back == NULL) {
         // The queue is empty, so both front and back point to the new element
-        qp->front = new_node;
-        qp->back = new_node;
+        iqp->front = new_node;
+        iqp->back = new_node;
     } else {
         // Add a new element to the end of the queue
-        qp->back->next = new_node;
-        qp->back = new_node;
+        iqp->back->next = new_node;
+        iqp->back = new_node;
     }
 
     return 0; // Success
@@ -77,21 +80,26 @@ int32_t qput(queue_t *qp, void *elementp) {
                                                                                            
                                                                                              
 /* get the first first element from queue, removing it from the queue */                     
-void* qget(queue_t *qp){                                                                                                                     
-  void* removed;                                                                                                                             
-  node_t* current;                                                                                                                           
-  if ((qp->front != NULL) && (qp->back != NULL)){                                                                                            
-    current = qp->front;                                                                                                                     
-    qp->front = current->next;                                                                                                               
-    removed = current;                                                                                                                       
-  }                                                                                                                                          
-  return removed;                                                                                                                            
-                                                                                                                                             
-}                                                                                             
+void* qget(queue_t *qp){ 
+  iqueue_t *iqp = (iqueue_t*) qp;                                                                      
+  if ((iqp->front != NULL) && (iqp->back != NULL)){                                            
+    void *removed = iqp->front;                                                               
+                                                                                             
+    node_t *current;                                                                         
+    current = iqp->front;                                                                     
+    iqp->front = current->next;                                                               
+                                                                                             
+    return removed;                                                                          
+  }      
+  else {
+  	return NULL; //Queue Empty
+  }                                                                                    
+}                                                                                            
                                                                                              
 /* apply a function to every element of the queue */                                         
-void qapply(queue_t *qp, void (*fn)(void* elementp)){                                        
-  node_t *current = qp->front;                                                               
+void qapply(queue_t *qp, void (*fn)(void* elementp)){  
+  iqueue_t *iqp = (iqueue_t*) qp;                                        
+  node_t *current = iqp->front;                                                               
   while (current != NULL) {                                                                                        
     fn(current->elementp);                                                                             
     current = current->next;                                                                 
@@ -108,11 +116,12 @@ void qapply(queue_t *qp, void (*fn)(void* elementp)){
  * returns a pointer to an element, or NULL if not found
  */
 void* qsearch(queue_t *qp, bool (*searchfn)(void* elementp, const void* keyp), const void* skeyp) {
-if (qp == NULL || searchfn == NULL || skeyp == NULL) {
+  iqueue_t *iqp = (iqueue_t*) qp;  
+if (iqp == NULL || searchfn == NULL || skeyp == NULL) {
         return NULL; // Invalid input
     }
 
-    node_t *current = qp->front;
+    node_t *current = iqp->front;
 
     while (current != NULL) {
         if (searchfn(current->elementp, skeyp)) {
@@ -127,26 +136,32 @@ if (qp == NULL || searchfn == NULL || skeyp == NULL) {
 /* concatenatenates elements of q2 into q1
  * q2 is dealocated, closed, and unusable upon completion 
  */
-void qconcat(queue_t *q1p, queue_t *q2p){  
-	
-  //if ((q1p->front != NULL)&&(q2p->front != NULL)){                              
-    q1p->back->next = q2p->front;                                               
-    q1p->back = q2p->back;                                                      
-  //}                                                                             
-  free(q2p);                                                                    
+void qconcat(queue_t *q1p, queue_t *q2p){     
+  iqueue_t *iq1p = (iqueue_t*) q1p;  
+    iqueue_t *iq2p = (iqueue_t*) q2p;                                                                    
+    if ((iq1p->front != NULL)&&(iq2p->front != NULL)){                                                            
+    iq1p->back->next = iq2p->front;                                                                               
+    iq1p->back = iq2p->back;                                                                                      
+      }                                                                                                         
+    else{                                                                                                       
+      iq1p->front = iq2p->front;                                                                                  
+      iq1p->back = iq2p->back;                                                                                    
+    }                                                                                                           
+  free(iq2p);                                                                                                    
 } 
 
 void* qremove(queue_t *qp, bool (*searchfn)(void* elementp,const void* keyp),const void* skeyp){
-	if (skeyp == NULL || qp == NULL) {
+	  iqueue_t *iqp = (iqueue_t*) qp;  
+	if (skeyp == NULL || iqp == NULL) {
         return NULL; // Invalid input or empty queue
     }
-    node_t *current = qp->front; 
+    node_t *current = iqp->front; 
     
     if (current != NULL && searchfn(current->elementp, skeyp)) {
         // Element to be removed is the front of the queue
-        qp->front = current->next;
-        if (current == qp->back) {
-            qp->back = NULL;
+        iqp->front = current->next;
+        if (current == iqp->back) {
+            iqp->back = NULL;
         }
         void* removed = current->elementp;
         free(current);
@@ -155,8 +170,8 @@ void* qremove(queue_t *qp, bool (*searchfn)(void* elementp,const void* keyp),con
     
     while (current != NULL && current->next != NULL) {
         if (searchfn(current->next->elementp, skeyp)) {
-            if (current->next == qp->back) {
-                qp->back = current;
+            if (current->next == iqp->back) {
+                iqp->back = current;
             }
             node_t *temp = current->next;
             current->next = current->next->next;
